a1f9e994b32e23322e937ff27a29d117ae3ffb6e
wert <wertdclemos@gmail.com>
2025-11-28 13:09:50 -0300

    Cadastro e consulta
    corrigido

--- cadastro.c
+++ cadastro.c
@@ -1,96 +1,195 @@
 #include <stdio.h>
 #include <string.h>
-
-struct cliente{
-	 int cpf;
-	 int contato;
-	 int compras;
-	 char nome[50];
-	 char email[50];
+#include <ctype.h>
+struct cliente {
+    char cpf[12];      
+    char nome[50];
+    char email[50];
+    char contato[20];
+    int compras;
 };
+int conferirCpf(char *cpfEntrada, FILE *arqPessoa) {
+    if (strlen(cpfEntrada) != 11) {
+        printf("CPF deve conter 11 dígitos!\n");
+        return 0;
+    }
+    for (int i = 0; i < 11; i++) {
+        if (!isdigit(cpfEntrada[i])) {
+            printf("CPF deve conter apenas números!\n");
+            return 0;
+        }
+    }
+    rewind(arqPessoa);
+    struct cliente temp;
 
-int conferirCpf(char* Ccpf){
-    int aux,n=10000000000,vetorCpf[11];
-	for(int i=0;i<11;i++){
-		aux=Ccpf->cpf/n;
-		vetorCpf[i]=aux;
-		aux=Ccpf->cpf%n;
-		n=/10;
-	}
+    while (fread(&temp, sizeof(struct cliente), 1, arqPessoa) == 1) {
+        if (strcmp(temp.cpf, cpfEntrada) == 0) {
+            printf("CPF já cadastrado!\n");
+            return 0;
+        }
+    }
+
+    return 1;
 }
-int conferirEmail(char* Eemail){
-	char gmail[11]='@gmail.com';
-	char hotmail[13]='@hotmail.com';
-	int retorno;
-	int count=0;
-	int tam=strlen(Eemail->email);
-	while(count<tam+1){
-		if(Eemail->email[count]=='@';){
-			retorno=strncmp(Eemail->email[count],gmail);
-				if(retorno==0){
-					break;
-				}
-			else{
-				retorno=strncmp(Eemail->email[count],hotmail);
-					if(retorno==0){
-						break;
-					}
-			}
-		}
-	}
-	return retorno;
+int conferirEmail(char *email) {
+
+    if (strchr(email, '@') == NULL)
+        return 0;
+
+    const char *dom1 = "@gmail.com";
+    const char *dom2 = "@hotmail.com";
+    const char *dom3 = "@outlook.com";
+
+    if (strstr(email, dom1) || strstr(email, dom2) || strstr(email, dom3))
+        return 1;
+
+    return 0;
+}
+void cadastro(struct cliente *P, int *qtd, FILE *arqPessoa) {
+    int valido = 0;
+
+    getchar(); 
+
+    printf("Nome do cliente: ");
+    fgets(P->nome, 50, stdin);
+    P->nome[strcspn(P->nome, "\n")] = '\0';
+    while (!valido) {
+        printf("CPF (11 dígitos): ");
+        fgets(P->cpf, 12, stdin);
+        P->cpf[strcspn(P->cpf, "\n")] = '\0';
+
+        valido = conferirCpf(P->cpf, arqPessoa);
+    }
+    printf("Contato (telefone): ");
+    fgets(P->contato, 20, stdin);
+    P->contato[strcspn(P->contato, "\n")] = '\0';
+    valido = 0;
+    while (!valido) {
+        printf("Email: ");
+        fgets(P->email, 50, stdin);
+        P->email[strcspn(P->email, "\n")] = '\0';
+
+        if (!conferirEmail(P->email)) {
+            printf("Email inválido! Aceito apenas @gmail.com, @hotmail.com ou @outlook.com.\n");
+        } else {
+            valido = 1;
+        }
+    }
+
+    printf("Quantidade de compras: ");
+    scanf("%d", &P->compras);
+    fseek(arqPessoa, 0, SEEK_END);
+    fwrite(P, sizeof(struct cliente), 1, arqPessoa);
+
+    (*qtd)++;
 }
 
-void cadastro (int* P){
-int controle=1;
+void listarClientes(FILE *arqPessoa) {
+    struct cliente temp;
 
-    printf("Nome do cliente: \n");
-	fgets(P->nome,50,stdin);
-	
-	while(controle==1){ //verifica se o cpf tem 11 dígitos//
-		printf("Cpf do cliente: \n");
-		scanf("%i",P->cpf);
-		controle=conferir(P->cpf);
-	} 
-	
-	controle=1;
-	
-	printf("Contato do cliente: \n");
-	scanf("%i",P->contato);
-	
-	 while(controle==1){
-		printf("E-mail do cliente: \n");
-		fgets(P->email,50,stdin);
-		controle=conferirEmail(P->email);
-	}
-		
-	 printf("Quantidade de compras do cliente: \n");
-	 scanf("%i",P->compras);
+    rewind(arqPessoa);
+
+    printf("\n=== CLIENTES CADASTRADOS ===\n");
+
+    while (fread(&temp, sizeof(struct cliente), 1, arqPessoa) == 1) {
+        printf("\n------------------------\n");
+        printf("Nome: %s\n", temp.nome);
+        printf("CPF: %s\n", temp.cpf);
+        printf("Contato: %s\n", temp.contato);
+        printf("Email: %s\n", temp.email);
+        printf("Compras: %d\n", temp.compras);
+    }
 }
 
-int main(){
-	 struct cliente pessoa;
-	 int escolha=-1;
-	 char yn[2];
-	 FILE* arqPessoa;
-	 arqPessoa=fopen("C:\\Área de trabalho\\clientes.txt","w");
-	 if(arqPessoa==NULL){
-		  printf("Erro na abertura do arquivo");
-		  return 1;
-	 }
-	 printf("\n\n=========MENU=========\n");
-	 while(escolha!=0){
-		  printf("Digite o número correspondente a opção que deseja.\n");
-		  printf("1) Cadastrar um cliente.\n");
-		  
-		  scanf("%i",&escolha);
-		  switch(escolha)
-		  case 1:
-				while(yn[0]!='N' || yn[0]!='n'){
-					 cadastro(&pessoa);
-					 fwrite(&pessoa,sizeof(struct pessoa),1,arqPessoa);
-					 printf("Deseja fazer um novo cadastro?[S/N]");
-					 scanf("%c",yn);
-				}
-	 }
+void buscarClientePorCPF(FILE *arqPessoa) {
+    char cpfBusca[12];
+    struct cliente temp;
+    int encontrado = 0;
+
+    printf("\nDigite o CPF para buscar: ");
+    getchar();
+    fgets(cpfBusca, 12, stdin);
+    cpfBusca[strcspn(cpfBusca, "\n")] = '\0';
+
+    if (strlen(cpfBusca) != 11) {
+        printf("CPF deve ter 11 dígitos!\n");
+        return;
+    }
+
+    rewind(arqPessoa);
+
+    while (fread(&temp, sizeof(struct cliente), 1, arqPessoa) == 1) {
+        if (strcmp(temp.cpf, cpfBusca) == 0) {
+            encontrado = 1;
+
+            printf("\n=== CLIENTE ENCONTRADO ===\n");
+            printf("Nome: %s\n", temp.nome);
+            printf("CPF: %s\n", temp.cpf);
+            printf("Contato: %s\n", temp.contato);
+            printf("Email: %s\n", temp.email);
+            printf("Compras: %d\n", temp.compras);
+            break;
+        }
+    }
+
+    if (!encontrado)
+        printf("Nenhum cliente encontrado com esse CPF.\n");
+}
+
+
+int main() {
+
+    struct cliente pessoa;
+    int escolha = -1, contador = 0;
+    char yn;
+
+    FILE *arqPessoa = fopen("clientes.dat", "ab+");
+
+    if (arqPessoa == NULL) {
+        printf("Erro ao abrir arquivo!\n");
+        return 1;
+    }
+
+    while (escolha != 0) {
+
+        printf("\n====== MENU ======\n");
+        printf("1) Cadastrar cliente\n");
+        printf("2) Listar clientes\n");
+        printf("3) Buscar cliente por CPF\n");
+        printf("0) Sair\n");
+        printf("Escolha: ");
+        scanf("%d", &escolha);
+
+        switch (escolha) {
+
+        case 1:
+            yn = 'S';
+            while (yn == 'S' || yn == 's') {
+                cadastro(&pessoa, &contador, arqPessoa);
+
+                printf("Cadastrar outro? (S/N): ");
+                getchar();
+                yn = getchar();
+            }
+            break;
+
+        case 2:
+            listarClientes(arqPessoa);
+            break;
+
+        case 3:
+            buscarClientePorCPF(arqPessoa);
+            break;
+
+        case 0:
+            printf("Saindo...\n");
+            break;
+
+        default:
+            printf("Opção inválida!\n");
+        }
+    }
+
+    fclose(arqPessoa);
+    return 0;
 }
